49ba9fbbc2d424de69199c86f97f3012a216f2c2
swetakarar <kararsweta@gmail.com>
2022-04-05 20:48:09 +0530

    fix: images expand bug onClick

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("img").click(function () {
+  $("#gallery img").click(function () {
     var t = $(this).attr("src");
     $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
     $("#myModal").modal();
